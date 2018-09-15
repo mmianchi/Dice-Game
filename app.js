2184f5b0978b8552468c60ef6387dda6389be1d5
mmianchi <mmianchi@gmail.com>
2018-09-15 00:17:09 -0700

    change bg

--- app.js
+++ app.js
@@ -1,56 +1,41 @@
 
-let scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 + 1);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
-
-let d = document.getElementById('score-'+ activePlayer ).textContent;
-d += dice;
-
-// const btnHold = document.querySelector('.btn-hold');
-// const btnRoll = document.querySelector('.btn-roll');
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
-// let score0 = document.querySelector('#score-0');
-// let name0 = document.querySelector('#name-0');
-// let current0 = document.querySelector('#current-0');
+document.querySelector('.btn-roll').addEventListener('click' , function(){
 
-// let score1 = document.querySelector('#score-1');
-// let name1 = document.querySelector('#name-1');
-// let current1 = document.querySelector('#current-1');
+  var dice = Math.floor(Math.random() * 6 + 1);
+  document.querySelector('#current-' + activePlayer).textContent = dice;
 
-// btnRoll.addEventListener('click', rolling);
-// btnHold.addEventListener('click', holding);
-
-// //every img is related to one score
-// let dice = document.querySelector('.dice').src 
-// var x = Math.floor(Math.random() * 6 + 1);
-// console.log(x)
-// function rolling(e){
-//   //chose random
+  var diceDom = document.querySelector('.dice')
+  //first change to block and cancle ivisible then give it a value
+  diceDom.style.display = 'block';
+  diceDom.src = 'dice-' + dice + '.png';
   
-//   //add it to related player score unless more than 1 
 
-//   //if score is 1 then reset current and change player
+  if (dice !== 1){
+    roundScore += dice;
+    document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+  } else {
+    //change player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0
 
-//   //if score> 100 related player = winner
+    document.querySelector('#current-0').textContent = '0';
+    document.querySelector('#current-1').textContent = '0';
+    
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active');
 
-//   e.preventDefault();
-// }
-
-// function holding(e) {
-  
-//   // add it to related score
-
-//   // change current to zero
-
-//   // change player 
-
-//   e.preventDefault();
-// }
+  }
+});
