4964fa2f08293eab14298640543b90ac95dc5f8e
mmianchi <mmianchi@gmail.com>
2019-01-27 04:25:08 -0800

    added init function

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click' , function(){
 
@@ -34,7 +26,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
   if (scores[activePlayer] >= 20) {
-    document.querySelector('#name-' + activePlayer).textContent = 'winner';
+    document.querySelector('#name-' + activePlayer).textContent = 'winner!!';
     document.querySelector('.dice').style.display = 'none';
 
     document.querySelector('.player-' + activePlayer +'-panel').classList.add('winner');
@@ -58,4 +50,22 @@ function nextPlayer(){
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
   document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  
+  
 }
