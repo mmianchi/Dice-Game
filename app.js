31aa17042a9f69661e0f830e31b154db4aeaaeba
mmianchi <mmianchi@gmail.com>
2019-01-27 04:00:18 -0800

    added hold function

--- app.js
+++ app.js
@@ -23,21 +23,27 @@ document.querySelector('.btn-roll').addEventListener('click' , function(){
     roundScore += dice;
     document.querySelector('#current-'+ activePlayer).textContent = roundScore;
   } else {
-    //change player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0
-
-    document.querySelector('#current-0').textContent = '0';
-    document.querySelector('#current-1').textContent = '0';
-    
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    diceDom.style.display = 'none';
+    nextPlayer()
   }
-
 
 });
 
-document.querySelector('.btn-hold').addEventListener('click', function () {
+document.querySelector('.btn-hold').addEventListener('click', function() {
   
+  scores[activePlayer]+= roundScore;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  nextPlayer()
 })
+
+
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0
+
+  document.querySelector('#current-0').textContent = '0';
+  document.querySelector('#current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  diceDom.style.display = 'none';
+}
